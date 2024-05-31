2ea4361d5375d3474f936d2e2129a5659e8261a2
Spied1 <117092326+Spied1@users.noreply.github.com>
2024-05-31 20:02:57 +0400

    added 39_and_41

---
+++ homework_39_41.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <utility>
+#include<string>
+#include <initializer_list>
+
+namespace nstd
+{
+	template<typename T>
+	class vector
+	{
+	private:
+		size_t m_capacity;
+		size_t m_size;
+		T* m_arr;
+	public:
+		vector(size_t size = 0) :
+			m_size(size),
+			m_capacity(m_size * 2 + 1),
+			m_arr(new T[m_capacity])
+		{
+
+		}
+
+		vector(const nstd::vector<T> & other)
+		{
+			m_capacity = other.m_capacity;
+			m_size = other.m_size;
+
+			m_arr = new T[m_capacity];
+
+			for (size_t i = 0; i < m_size; ++i)
+			{
+				m_arr[i] = other.m_arr[i];
+			}
+		}
+
+		vector(std::initializer_list<T> elements) :
+			m_arr(new T[elements.size()])
+		{
+			for (const T& i : elements)
+			{
+				push_back(i);
+			}
+		}
+
+		vector& operator=(nstd::vector<T>& other)
+		{
+			this->m_capacity = other.m_capacity;
+			this->m_size = other.m_size;
+
+			this->m_arr = new T[m_capacity];
+
+			for (size_t i = 0; i < this->m_size; ++i)
+			{
+				this->m_arr[i] = other.m_arr[i];
+			}
+		}
+
+		void reserve(size_t new_size)
+		{
+			if (new_size < m_capacity)
+			{
+				return;
+			}
+
+
+			T* temp = new T[new_size]{};
+
+			for (size_t i = 0; i < m_size; ++i)
+			{
+				temp[i] = m_arr[i];
+			}
+
+			delete[] m_arr;
+			m_arr = temp;
+			m_capacity = new_size;
+		}
+
+		void push_back(const T& element)
+		{
+			if (m_size == m_capacity)
+			{
+				reserve(m_size * 2 + 1);
+			}
+
+			m_arr[m_size] = element;
+			m_size++;
+		}
+
+		void push_front(const T& element)
+		{
+			push_back(element);
+			for (size_t i = m_size - 1; i > 0; --i)
+			{
+				std::swap(m_arr[i], m_arr[i - 1]);
+			}
+		}
+
+		void pop_back()
+		{
+			if (m_size == 0)
+			{
+				return;
+			}
+
+			m_size--;
+		}
+
+		void pop_front()
+		{
+			for (size_t i = 0; i < m_size - 1; ++i)
+			{
+				std::swap(m_arr[i],m_arr[i + 1]);
+			}
+			
+			pop_back();
+		}
+
+		void insert(size_t pos, const T& element)
+		{
+			push_back(element);
+
+			for (size_t i = m_size - 1; i > pos + 1; --i)
+			{
+				std::swap(m_arr[i], m_arr[i - 1]);
+			}
+		}
+
+		bool empty()
+		{
+			return !m_size;
+		}
+
+		const size_t& size()
+		{
+			return m_size;
+		}
+
+		T& operator[](const size_t& index)
+		{
+			return m_arr[index];
+		}
+
+		void clear()
+		{
+			m_size = 0;
+		}
+
+		~vector()
+		{
+			delete[] m_arr;
+		}
+	};
+}
+
+void print(nstd::vector<int>& vec)
+{
+	for (int i = 0; i < vec.size(); ++i)
+	{
+		std::cout << vec[i] << " ";
+	}
+
+}
+
+int main()
+{
+	nstd::vector<int> a = { 1,2,3,4,5,6,7,8,9,10 };
+	nstd::vector<int> k(a);
+
+	print(k);
+	
+	std::cout << std::endl;
+
+	k.pop_front();
+
+	print(k);
+	std::cout << std::endl;
+
+	k.push_front(111);
+
+	print(k);
+	std::cout << std::endl;
+
+	k.insert(3,222);
+	print(k);
+}
